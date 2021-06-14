727b4109599a2ed00042a0ad8e175bf6fbadadc8
hemanta212 <sharmahemanta.212@gmail.com>
2021-06-14 22:06:28 +0545

    Adds pset 4 recover problem and minor update to lab4

--- lab4/volume.c
+++ lab4/volume.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef uint16_t HEADERBYTE;
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[]) {
   const int HEADER_SIZE = 44;
- 
-  if (argc != 4){
+
+  if (argc != 4) {
     fprintf(stderr, "Usage: ./volume input.wav output.wav factor\n");
     return 1;
   }
@@ -13,35 +16,30 @@ int main(int argc, char *argv[]) {
   float factor = atof(argv[3]);
   FILE *file = fopen(argv[1], "r");
   FILE *to_file = fopen(argv[2], "w");
-  if (!file){
+  if (file == NULL) {
     fprintf(stderr, "Failed to open file!\n");
     return 1;
   }
 
-  if (!to_file) {
+  if (to_file == NULL) {
     fclose(file);
     fprintf(stderr, "Failed to copy!\n");
     return 1;
   }
 
-  uint8_t headers[HEADER_SIZE];
-  int16_t sample;
-  int counter = 0;
-  while (1) {
-    if (counter < HEADER_SIZE) {
-      fread(headers, sizeof(uint8_t), 1, file);
-      fwrite(headers, sizeof(uint8_t), 1, to_file);
-    } else {
-      int read = fread(&sample, sizeof(int16_t), 1, file);
-      if(!read)
-	break;
-      sample = sample * factor;
-      fwrite(&sample, sizeof(uint16_t), 1, to_file);
-    }
-    counter++;
+  // the headers and body bits are two types you get in a binary file
+  // extracting the headers
+  HEADERBYTE headers[HEADER_SIZE];
+  fread(&headers, sizeof(HEADERBYTE), HEADER_SIZE, file);
+  fwrite(&headers, sizeof(HEADERBYTE), HEADER_SIZE, to_file);
+
+  BYTE sample;
+  while (fread(&sample, sizeof(BYTE), 1, file) == 1) {
+    sample *= factor;
+    fwrite(&sample, sizeof(BYTE), 1, to_file);
   }
 
-  fclose(file);
   fclose(to_file);
+  fclose(file);
   return 0;
 }
---
+++ pset4/recover/recover.c
@@ -0,0 +1,80 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define BLOCK_SIZE 512
+
+typedef uint8_t BYTE;
+
+void get_next_filename(int file_count, char *filename);
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: ./recover file.raw\n");
+    return 1;
+  }
+  // Get the file command arg
+  char *file_arg = argv[1];
+
+  // prerpare the card raw file to open else just close and return
+  FILE *raw_file = fopen(file_arg, "r");
+  if (raw_file == NULL) {
+    fprintf(stderr, "Couldnot read file %s\n", file_arg);
+    return 1;
+  }
+
+  // couner for keeping tarck of nu of files and help name them
+  int file_count = 0;
+  FILE *current_file = NULL;
+
+  // Instead of scanning each bytes we count in batch of 512
+  BYTE block[BLOCK_SIZE];
+
+  while (fread(block, sizeof(BYTE), BLOCK_SIZE, raw_file) == BLOCK_SIZE) {
+    // last byte of 4 jpg byte ranges from 0xe0-0xef so we make sure it stats
+    // with 0xe
+    int jpeg_last = block[3] / 16 == 0xe;
+
+    // check if we found a starting mark of jpeg
+    if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && jpeg_last) {
+      // Abort writing to current file if there is one
+      if (current_file != NULL)
+        fclose(current_file);
+
+      // ### for filename and .jpg for ext and \0 --> total 8
+      char filename[8];
+
+      // Get the next filename
+      get_next_filename(file_count, filename);
+
+      // Open the next file
+      current_file = fopen(filename, "w");
+      if (current_file == NULL) {
+        fclose(raw_file);
+	fprintf(stderr, "Couldnot write to file %s\n", filename);
+        return 1;
+      }
+
+      file_count++;
+    }
+
+    // If we didnot encounter jpeg demarkation in this block just continue
+    // writing to current file
+    if (current_file != NULL) {
+      fwrite(block, sizeof(BYTE), BLOCK_SIZE, current_file);
+    }
+  }
+
+  // the current image output file wont close on the last block
+  fclose(current_file);
+  fclose(raw_file);
+  return 0;
+}
+
+void get_next_filename(int file_count, char *filename) {
+  if (file_count < 10)
+    sprintf(filename, "00%d.jpg", file_count);
+  else if (file_count < 100)
+    sprintf(filename, "0%d.jpg", file_count);
+  else
+    sprintf(filename, "%d.jpg", file_count);
+}
